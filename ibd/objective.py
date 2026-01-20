e8e0f2aa935f27bfc53436ec87f3a5272d0face4
Ashton Liam Brown <ashtonliambrown5@gmail.com>
2026-01-20 20:36:26 -0300

    Implement numerical derivative in ObjectiveFunction
    Added a derivative method for numerical differentiation.

--- ibd/objective.py
+++ ibd/objective.py
@@ -8,3 +8,21 @@ class ObjectiveFunction:
 
     def evaluate(self, x):
         return self.func(x)
+import numpy as np
+
+class ObjectiveFunction:
+    """
+    Wraps a mathematical objective function.
+    """
+
+    def __init__(self, func):
+        self.func = func
+
+    def evaluate(self, x):
+        return self.func(x)
+
+    def derivative(self, x, h=1e-5):
+        """
+        Numerical derivative (finite differences).
+        """
+        return (self.func(x + h) - self.func(x - h)) / (2 * h)
